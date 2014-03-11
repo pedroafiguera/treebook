f90e609fe771c47fa0c303139f8156c9d059ea86
Pedro Figuera <pedroafiguera@gmail.com>
2014-03-10 21:42:03 -0400

    Generated profile controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Treebook::Application.routes.draw do
-  devise_for :users
-  resources :statuses
-
+  
+  get "profiles/show"
   root :to => "statuses#index"
+
+  devise_for :users
+
+	devise_scope :user do
+		get "sign_up", to: "devise/registrations#new"
+	  get "sign_in", to: "devise/sessions#new"	  
+	  get "sign_out", to: "devise/sessions#destroy"
+	end 
+
+	resources :statuses
+
+	get "feed", to: "statuses#index"	 
 end
